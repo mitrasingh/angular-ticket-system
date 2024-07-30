52713b5d5bfa6442d6293bf12344d09e64f10695
mitrasingh <mitra.coder@gmail.com>
2024-07-30 18:40:05 -0400

    update: import DashboardItemComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
 import { TrafficComponent } from './dashboard/traffic/traffic.component';
 import { SupportTicketsComponent } from './dashboard/support-tickets/support-tickets.component';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ import { SupportTicketsComponent } from './dashboard/support-tickets/support-tic
     ServerStatusComponent,
     TrafficComponent,
     SupportTicketsComponent,
+    DashboardItemComponent,
   ],
 })
 export class AppComponent {
