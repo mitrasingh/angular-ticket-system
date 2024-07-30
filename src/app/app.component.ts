d28ad5bd193093fd8416c7c011a0ae72873a553d
mitrasingh <mitra.coder@gmail.com>
2024-07-29 20:55:15 -0400

    update: add support tickets component to imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './server-status/server-status.component';
+import { TrafficComponent } from './traffic/traffic.component';
+import { SupportTicketsComponent } from './support-tickets/support-tickets.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent],
+  imports: [
+    HeaderComponent,
+    ServerStatusComponent,
+    TrafficComponent,
+    SupportTicketsComponent,
+  ],
 })
 export class AppComponent {
   dummyTrafficData = [
