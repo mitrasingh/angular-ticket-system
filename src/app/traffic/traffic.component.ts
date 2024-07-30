bdf2dd357ea8ff7638325ad3e8a03d11c07c8578
mitrasingh <mitra.coder@gmail.com>
2024-07-29 20:41:53 -0400

    update: add input data from parent, add type for shape of object received from parent

--- src/app/traffic/traffic.component.ts
+++ src/app/traffic/traffic.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+type dummyTrafficData = {
+  id: string;
+  value: number;
+};
 @Component({
   selector: 'app-traffic',
   standalone: true,
   imports: [],
   templateUrl: './traffic.component.html',
-  styleUrl: './traffic.component.css'
+  styleUrl: './traffic.component.css',
 })
 export class TrafficComponent {
-
+  @Input({ required: true }) dummyTrafficData!: dummyTrafficData[];
 }
