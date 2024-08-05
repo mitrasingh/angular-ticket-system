bb94b1a8cd1de6141a976a3c611c68e0124a60df
mitrasingh <mitra.coder@gmail.com>
2024-08-04 22:12:02 -0400

    update: add Input decorator for title

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
 })
 export class ControlComponent {
-
+  // Could use ng-content but using @Input instead
+  @Input({ required: true }) label!: string;
 }
