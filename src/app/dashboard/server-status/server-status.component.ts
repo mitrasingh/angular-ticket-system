4bf83cd3e26d57e12d51de10cb8ee52b292b677e
mitrasingh <mitra.coder@gmail.com>
2024-08-05 09:35:27 -0400

    update: add currentStatus property and value

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -8,5 +8,5 @@ import { Component, Input } from '@angular/core';
   styleUrl: './server-status.component.css',
 })
 export class ServerStatusComponent {
-  @Input({ required: true }) currentStatus!: string;
+  currentStatus = 'online';
 }
