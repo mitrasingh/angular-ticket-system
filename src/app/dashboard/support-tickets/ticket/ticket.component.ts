5c2f38999559f6f55a6903cd979ef179e411d4a0
mitrasingh <mitra.coder@gmail.com>
2024-08-13 23:23:49 -0400

    add: Output decorator with event emittion, onMarkAsCompleted method created for emit

--- src/app/dashboard/support-tickets/ticket/ticket.component.ts
+++ src/app/dashboard/support-tickets/ticket/ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Ticket } from '../ticket.model';
 
 @Component({
@@ -11,11 +11,16 @@ import { Ticket } from '../ticket.model';
 export class TicketComponent {
   @Input({ required: true }) data!: Ticket;
   @Input() detailsVisible: boolean = false;
+  @Output() close = new EventEmitter<string>();
 
   // detailsVisible = signal(false);
 
   onToggleDetails() {
     // this.detailsVisible.update((wasVisible) => !wasVisible);
     this.detailsVisible = !this.detailsVisible;
+  }
+
+  onMarkAsCompleted() {
+    this.close.emit();
   }
 }
